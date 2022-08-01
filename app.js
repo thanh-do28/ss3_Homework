c60f180d480d48f2ccac22bad3e3ebbb0f8c5878
thanh-do28 <96042778+thanh-do28@users.noreply.github.com>
2022-08-02 00:22:43 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+// câu 1
+// for (let i = 1; i < 100; i++) {
+//     console.log(`${i},`);
+//     if (i === 99) {
+//         console.log('đã kết thúc');
+//     }
+// }
+
+//câu 2
+// let a = 0;
+// let b = 1;
+// let c;
+// let e = '';
+// for (let i = 1; i <= 20; i++) {
+//         e += `${a},`;
+//         c = a + b;
+//         a = b;
+//         b = c;
+// }console.log(e);
+
+
+//câu 3
+// let a = 0;
+// let b = 1;
+// let c;
+// for (let i = 1; i <= 20; i++) {
+//     if (a % 5 ===0){
+//         console.log(a);
+//         break;
+//     }
+//     c = a + b;
+//     a = b;
+//     b = c;
+// }
+
+
+//câu 4
+// let a = 0;
+// let b = 1;
+// let c;
+// let e = 0;
+// for (let i = 1; i <= 20; i++) {
+//         e += a;
+//         c = a + b;
+//         a = b;
+//         b = c;
+// }console.log(e);
+
+
+// câu 5
+
+// let i=1;
+// let dem=0;
+// let tong=0;
+// while(dem<=29){
+//     if(i%7==0){
+//         dem+=1;
+//         tong+=i;
+//     }
+// i+=1;
+// }
+// console.log(tong);
+
+
+//câu 6
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log( i + "FizzBuzz");
+//     } else if (i % 3 == 0) {
+//         console.log(i + "Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log(i + "Buzz");
+//     }
+// }
+
+// câu 7
+// let a = Number(prompt('nhập số n'));
+// let b = 1;
+// for (let i = a; i >= 1; i--) {
+//     b = b * i;
+// }console.log(b);
+
